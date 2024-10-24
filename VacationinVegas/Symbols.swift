83841b3bee835e699caa9e3ff3ff50280b6a78e9
Rodrigo Monteiro <rmonteir@icloud.com>
2024-10-24 11:47:37 -0300

    Added new symbols file

---
+++ VacationinVegas/Symbols.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  VacationinVegas
+//
+//  Created by Rodrigo Monteiro on 19/10/24.
+//
+
+import SwiftUI
+
+struct Symbols: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
